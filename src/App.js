490d76862567214ae6578600f5630f06c16efcd0
Jaryd Krishnan <jaryd@hey.com>
2022-06-07 14:59:53 -0500

    Add giphy example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Posts from './components/Posts';
@@ -6,8 +7,24 @@ import Post from './components/Post';
 
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const getImageUrl = () => {
+    axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API}&limit=5&offset=5`).then((response) => {
+      const bigResponse = response.data;
+      const randomIndex = Math.floor(Math.random() * bigResponse.data.length);
+      setImageUrl(bigResponse.data[randomIndex].images.original.url);
+    }).catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getImageUrl();
+  }, [])
   return (
-    <div class="container">
+    <div className="container">
+      <header>
+        <img src={imageUrl} alt="Giphy" style={{ height: '100px', maxWidth: '100px' }} />
+        <button className="btn btn-success" onClick={getImageUrl}>Get New Image</button>
+      </header>
       <Router>
         <Routes>
           <Route path="/posts/:postId" element={<Post />} />
